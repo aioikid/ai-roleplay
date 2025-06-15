275ea3503c9e5365325bcc518133c2aa1110e3f2
aioikid <aioikid@gmail.com>
2025-06-16 00:49:23 +0900

    Fix Next.js 14 Netlify Build Configuration

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  output: 'export',
-  images: {
-    unoptimized: true
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: '/.netlify/functions/:path*'
-      }
-    ]
-  }
+  output: 'standalone'
 }
 
 module.exports = nextConfig
