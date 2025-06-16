d10c09e429fc79ddb7a03bff7efed6930af68572
aioikid <aioikid@gmail.com>
2025-06-16 23:57:42 +0900

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false,
-  experimental: {
-    turbo: {
-      enabled: true
-    }
-  },
-  compiler: {
-    // これで完全にSWCを無効化（Babelに戻す）
-    legacyBabel: true
-  }
+  swcMinify: false,  // SWC無効
+  compiler: {},
+  experimental: {}
 }
 
 module.exports = nextConfig
